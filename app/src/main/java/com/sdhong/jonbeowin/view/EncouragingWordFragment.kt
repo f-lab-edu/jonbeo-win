abb96177faf457f39679356f1f89ce26e7d915d0
Seongdeok Hong <sdhong0609@gmail.com>
2025-03-20 21:27:31 +0900

    style: reformat code

--- app/src/main/java/com/sdhong/jonbeowin/view/EncouragingWordFragment.kt
+++ app/src/main/java/com/sdhong/jonbeowin/view/EncouragingWordFragment.kt
@@ -5,12 +5,11 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.sdhong.jonbeowin.R
 import com.sdhong.jonbeowin.databinding.FragmentEncouragingWordBinding
 
 class EncouragingWordFragment : Fragment() {
 
-    private var _binding : FragmentEncouragingWordBinding? = null
+    private var _binding: FragmentEncouragingWordBinding? = null
     private val binding get() = _binding!!
 
     override fun onCreateView(
