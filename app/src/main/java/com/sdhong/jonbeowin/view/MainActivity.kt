4153f0d107f603ca1a2103e60604ee42d0dc9a3d
Seongdeok Hong <sdhong0609@gmail.com>
2025-03-20 19:21:54 +0900

    refactor: MainActivity 패키지 변경

--- app/src/main/java/com/sdhong/jonbeowin/view/MainActivity.kt
+++ app/src/main/java/com/sdhong/jonbeowin/view/MainActivity.kt
@@ -1,10 +1,11 @@
-package com.sdhong.jonbeowin
+package com.sdhong.jonbeowin.view
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.sdhong.jonbeowin.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
