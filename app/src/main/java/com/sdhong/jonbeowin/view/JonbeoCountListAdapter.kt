fda2910cd9352e2503eb66e87a6b450954487f81
Seongdeok Hong <sdhong0609@gmail.com>
2025-03-22 15:49:16 +0900

    feat: 존버 카운트 편집 모드
    feat: 존버 카운트 편집모드
    
    feat: 자산 편집

--- app/src/main/java/com/sdhong/jonbeowin/view/JonbeoCountListAdapter.kt
+++ app/src/main/java/com/sdhong/jonbeowin/view/JonbeoCountListAdapter.kt
@@ -1,33 +1,38 @@
 package com.sdhong.jonbeowin.view
 
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil.ItemCallback
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.sdhong.jonbeowin.databinding.ItemAssetBinding
 import com.sdhong.jonbeowin.local.model.Asset
+import com.sdhong.jonbeowin.view.uistate.AssetUiState
 
 class JonbeoCountListAdapter(
     private val onAssetItemClick: (asset: Asset) -> Unit
-) : ListAdapter<Asset, JonbeoCountViewHolder>(
-    object : ItemCallback<Asset>() {
-        override fun areItemsTheSame(oldItem: Asset, newItem: Asset): Boolean =
-            oldItem.id == newItem.id
+) : ListAdapter<AssetUiState, JonbeoCountViewHolder>(
+    object : ItemCallback<AssetUiState>() {
+        override fun areItemsTheSame(oldItem: AssetUiState, newItem: AssetUiState): Boolean =
+            oldItem.asset.id == newItem.asset.id
 
-        override fun areContentsTheSame(oldItem: Asset, newItem: Asset): Boolean =
+        override fun areContentsTheSame(oldItem: AssetUiState, newItem: AssetUiState): Boolean =
             oldItem == newItem
     }
 ) {
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): JonbeoCountViewHolder {
         val binding = ItemAssetBinding.inflate(LayoutInflater.from(parent.context), parent, false)
         val viewHolder = JonbeoCountViewHolder(binding)
-        binding.root.setOnClickListener {
+        val onClick = { _: View ->
             val position = viewHolder.absoluteAdapterPosition
             if (position != RecyclerView.NO_POSITION) {
-                onAssetItemClick(getItem(position))
+                onAssetItemClick(getItem(position).asset)
             }
         }
+        binding.root.setOnClickListener(onClick)
+        binding.checkboxEdit.setOnClickListener(onClick)
         return viewHolder
     }
 
