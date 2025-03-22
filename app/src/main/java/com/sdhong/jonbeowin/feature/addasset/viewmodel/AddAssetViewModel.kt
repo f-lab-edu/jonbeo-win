c20b89e811809c04d9629dcffa2f27577d6e0ff6
Seongdeok Hong <sdhong0609@gmail.com>
2025-03-22 23:07:16 +0900

    refactor: AddAssetUiState로 리팩토링

--- app/src/main/java/com/sdhong/jonbeowin/feature/addasset/viewmodel/AddAssetViewModel.kt
+++ app/src/main/java/com/sdhong/jonbeowin/feature/addasset/viewmodel/AddAssetViewModel.kt
@@ -5,14 +5,19 @@ import androidx.annotation.StringRes
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.sdhong.jonbeowin.R
+import com.sdhong.jonbeowin.feature.addasset.uistate.AddAssetUiState
 import com.sdhong.jonbeowin.feature.assetdetail.model.BuyDate
 import com.sdhong.jonbeowin.local.dao.AssetDao
 import com.sdhong.jonbeowin.local.model.Asset
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.receiveAsFlow
+import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -21,11 +26,25 @@ class AddAssetViewModel @Inject constructor(
     private val assetDao: AssetDao
 ) : ViewModel() {
 
-    private val _buyDate = MutableStateFlow(BuyDate.Default)
-    val buyDate = _buyDate.asStateFlow()
+    private val buyDate = MutableStateFlow(BuyDate.Default)
+
+    val uiState: StateFlow<AddAssetUiState> = buyDate.map { buyDate ->
+        if (buyDate == BuyDate.Default) {
+            AddAssetUiState.Idle
+        } else {
+            AddAssetUiState.Success(buyDate)
+        }
+    }.catch {
+        emit(AddAssetUiState.Error)
+    }.stateIn(
+        scope = viewModelScope,
+        started = SharingStarted.WhileSubscribed(5000),
+        initialValue = AddAssetUiState.Idle
+    )
 
     private val _eventChannel = Channel<AddAssetEvent>(Channel.BUFFERED)
     val eventFlow = _eventChannel.receiveAsFlow()
+
 
     fun saveAsset(assetName: String) {
         viewModelScope.launch {
@@ -39,25 +58,25 @@ class AddAssetViewModel @Inject constructor(
                 Asset(
                     name = assetName,
                     dayCount = diffDays + 1,
-                    buyDateString = _buyDate.value.formattedString,
+                    buyDateString = buyDate.value.formattedString,
                     generatedTime = Calendar.getInstance().time.toString()
                 )
             )
-            finishAddAsset()
+            eventFinishAddAsset()
         }
     }
 
-    private suspend fun validateAssetName(assetName: String): Boolean {
+    private fun validateAssetName(assetName: String): Boolean {
         if (assetName.isBlank()) {
-            _eventChannel.send(AddAssetEvent.ShowToast(R.string.asset_name_empty_message))
+            eventShowToast(R.string.asset_name_empty_message)
             return true
         }
         return false
     }
 
-    private suspend fun checkUserSetBuyDate(): Boolean {
-        if (_buyDate.value == BuyDate.Default) {
-            _eventChannel.send(AddAssetEvent.ShowToast(R.string.date_empty_message))
+    private fun checkUserSetBuyDate(): Boolean {
+        if (buyDate.value == BuyDate.Default) {
+            eventShowToast(R.string.date_empty_message)
             return true
         }
         return false
@@ -65,7 +84,7 @@ class AddAssetViewModel @Inject constructor(
 
     private fun getDiffDays(): Int {
         val buyDay = Calendar.getInstance().apply {
-            set(_buyDate.value.year, _buyDate.value.month - 1, _buyDate.value.day, 0, 0, 0)
+            set(buyDate.value.year, buyDate.value.month - 1, buyDate.value.day, 0, 0, 0)
             set(Calendar.MILLISECOND, 0)
         }.timeInMillis
         val today = Calendar.getInstance().apply {
@@ -78,25 +97,31 @@ class AddAssetViewModel @Inject constructor(
         return diffDays
     }
 
-    private suspend fun validateDiffDays(diffDays: Int): Boolean {
+    private fun validateDiffDays(diffDays: Int): Boolean {
         if (diffDays < 0) {
-            _eventChannel.send(AddAssetEvent.ShowToast(R.string.date_error_message))
+            eventShowToast(R.string.date_error_message)
             return true
         }
         return false
     }
 
     fun setBuyDate(year: Int, month: Int, day: Int) {
-        _buyDate.value = BuyDate(
+        buyDate.value = BuyDate(
             year = year,
             month = month,
             day = day
         )
     }
 
-    fun finishAddAsset() {
+    fun eventFinishAddAsset() {
         viewModelScope.launch {
             _eventChannel.send(AddAssetEvent.FinishAddAsset)
+        }
+    }
+
+    fun eventShowToast(@StringRes messageId: Int) {
+        viewModelScope.launch {
+            _eventChannel.send(AddAssetEvent.ShowToast(messageId))
         }
     }
 
