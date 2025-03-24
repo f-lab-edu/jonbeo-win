6ef6defdf9dac96f7d23cfaf1447bfcabd0d413b
Seongdeok Hong <sdhong0609@gmail.com>
2025-03-25 01:18:40 +0900

    refactor: 사용하지 않는 코드 제거

--- app/src/main/java/com/sdhong/jonbeowin/feature/addasset/uistate/AddAssetUiState.kt
+++
@@ -1,9 +0,0 @@
-package com.sdhong.jonbeowin.feature.addasset.uistate
-
-import com.sdhong.jonbeowin.local.model.BuyDate
-
-sealed interface AddAssetUiState {
-    data object Idle : AddAssetUiState
-    data class Success(val buyDate: BuyDate) : AddAssetUiState
-    data object Error : AddAssetUiState
-}
