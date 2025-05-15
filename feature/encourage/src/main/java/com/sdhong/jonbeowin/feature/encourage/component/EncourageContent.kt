1b14b69789a62e99d6e20ff0ff09c708884302b2
Seongdeok Hong <sdhong0609@gmail.com>
2025-05-16 01:50:50 +0900

    refactor: 기존 toolbar를 compose로 마이그레이션

--- feature/encourage/src/main/java/com/sdhong/jonbeowin/feature/encourage/component/EncourageContent.kt
+++ feature/encourage/src/main/java/com/sdhong/jonbeowin/feature/encourage/component/EncourageContent.kt
@@ -1,7 +1,13 @@
 package com.sdhong.jonbeowin.feature.encourage.component
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -11,6 +17,7 @@ import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.sdhong.jonbeowin.feature.encourage.R
 import com.sdhong.jonbeowin.feature.encourage.uistate.EncourageUiState
 
@@ -18,44 +25,64 @@ import com.sdhong.jonbeowin.feature.encourage.uistate.EncourageUiState
 internal fun EncourageContent(
     uiState: EncourageUiState,
     onEncourageItemClick: (Int) -> Unit,
-    onGenerateButtonClick: () -> Unit
+    onGenerateButtonClick: () -> Unit,
+    toggleEditMode: () -> Unit
 ) {
-    Box(
-        modifier = Modifier.fillMaxSize(),
-        contentAlignment = Alignment.Center
-    ) {
-        when (uiState) {
-            EncourageUiState.Idle -> Unit
-
-            EncourageUiState.Empty -> {
-                Text(
-                    text = stringResource(R.string.encourage_list_empty_message),
-                    color = colorResource(R.color.dusk_gray)
-                )
-            }
-
-            is EncourageUiState.Success -> {
-                EncourageList(
-                    items = uiState.encourageItemList,
+    Column {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(colorResource(R.color.light_blue))
+                .height(64.dp)
+                .padding(start = 16.dp),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(text = stringResource(R.string.title_encouraging_word), fontSize = 22.sp)
+            if (uiState is EncourageUiState.Success) {
+                EncourageTopAppBarActionButton(
                     isEditMode = uiState.isEditMode,
-                    onEncourageItemClick = onEncourageItemClick
-                )
-            }
-
-            EncourageUiState.Error -> {
-                Text(
-                    text = stringResource(R.string.encourage_list_error_message),
-                    color = colorResource(R.color.red)
+                    onClick = toggleEditMode
                 )
             }
         }
+        Box(
+            modifier = Modifier.fillMaxSize(),
+            contentAlignment = Alignment.Center
+        ) {
+            when (uiState) {
+                EncourageUiState.Idle -> Unit
 
-        GenerateEncourageButton(
-            modifier = Modifier
-                .align(Alignment.BottomEnd)
-                .padding(bottom = 16.dp, end = 16.dp),
-            onClick = onGenerateButtonClick
-        )
+                EncourageUiState.Empty -> {
+                    Text(
+                        text = stringResource(R.string.encourage_list_empty_message),
+                        color = colorResource(R.color.dusk_gray)
+                    )
+                }
+
+                is EncourageUiState.Success -> {
+                    EncourageList(
+                        items = uiState.encourageItemList,
+                        isEditMode = uiState.isEditMode,
+                        onEncourageItemClick = onEncourageItemClick
+                    )
+                }
+
+                EncourageUiState.Error -> {
+                    Text(
+                        text = stringResource(R.string.encourage_list_error_message),
+                        color = colorResource(R.color.red)
+                    )
+                }
+            }
+
+            GenerateEncourageButton(
+                modifier = Modifier
+                    .align(Alignment.BottomEnd)
+                    .padding(bottom = 16.dp, end = 16.dp),
+                onClick = onGenerateButtonClick
+            )
+        }
     }
 }
 
@@ -65,6 +92,7 @@ private fun EncourageContentPreview() {
     EncourageContent(
         uiState = EncourageUiState.Error,
         onEncourageItemClick = {},
-        onGenerateButtonClick = {}
+        onGenerateButtonClick = {},
+        toggleEditMode = {}
     )
 }
---
+++ feature/encourage/src/main/java/com/sdhong/jonbeowin/feature/encourage/component/EncourageTopAppBarActionButton.kt
@@ -0,0 +1,55 @@
+package com.sdhong.jonbeowin.feature.encourage.component
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.minimumInteractiveComponentSize
+import androidx.compose.material3.ripple
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.sdhong.jonbeowin.feature.encourage.R
+
+@Composable
+internal fun EncourageTopAppBarActionButton(
+    isEditMode: Boolean,
+    onClick: () -> Unit
+) {
+    Box(
+        modifier = Modifier.minimumInteractiveComponentSize()
+            .size(40.dp)
+            .clip(CircleShape)
+            .clickable(
+                interactionSource = null,
+                onClick = onClick,
+                indication = ripple()
+            ),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(
+            text = stringResource(
+                if (isEditMode) R.string.remove
+                else R.string.edit
+            ),
+            color = colorResource(R.color.blue),
+            style = MaterialTheme.typography.labelLarge
+        )
+    }
+}
+
+@Preview
+@Composable
+private fun EncourageTopAppBarActionButtonPreview() {
+    EncourageTopAppBarActionButton(
+        isEditMode = false,
+        onClick = {}
+    )
+}
