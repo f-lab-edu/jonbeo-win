1e78340e0fc7a2dc66d7f7712311b01b0c85700f
Seongdeok Hong <sdhong0609@gmail.com>
2025-06-03 08:15:09 +0900

    build: ConfigureKotlin

--- build-logic/src/main/java/AndroidLibraryPlugin.kt
+++ build-logic/src/main/java/AndroidLibraryPlugin.kt
@@ -1,13 +1,9 @@
 import com.android.build.gradle.LibraryExtension
-import com.sdhong.jonbeowin.libs
-import org.gradle.api.JavaVersion
+import com.sdhong.jonbeowin.configureKotlinAndroid
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.kotlin.dsl.apply
 import org.gradle.kotlin.dsl.configure
-import org.gradle.kotlin.dsl.dependencies
-import org.jetbrains.kotlin.gradle.dsl.JvmTarget
-import org.jetbrains.kotlin.gradle.dsl.KotlinAndroidProjectExtension
 
 class AndroidLibraryPlugin : Plugin<Project> {
     override fun apply(target: Project) {
@@ -16,11 +12,9 @@ class AndroidLibraryPlugin : Plugin<Project> {
             apply(plugin = "org.jetbrains.kotlin.android")
 
             extensions.configure<LibraryExtension> {
-                compileSdk = 35
+                configureKotlinAndroid(this)
 
                 defaultConfig {
-                    minSdk = 28
-
                     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
                     consumerProguardFiles("consumer-rules.pro")
                 }
@@ -31,19 +25,6 @@ class AndroidLibraryPlugin : Plugin<Project> {
                         proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
                     }
                 }
-                compileOptions {
-                    sourceCompatibility = JavaVersion.VERSION_11
-                    targetCompatibility = JavaVersion.VERSION_11
-                }
-                configure<KotlinAndroidProjectExtension> {
-                    compilerOptions.jvmTarget.set(JvmTarget.JVM_11)
-                }
-            }
-
-            dependencies {
-                "testImplementation"(libs.findLibrary("junit").get())
-                "androidTestImplementation"(libs.findLibrary("androidx-junit").get())
-                "androidTestImplementation"(libs.findLibrary("androidx-espresso-core").get())
             }
         }
     }
---
+++ build-logic/src/main/java/com/sdhong/jonbeowin/ConfigureKotlin.kt
@@ -0,0 +1,49 @@
+package com.sdhong.jonbeowin
+
+import com.android.build.api.dsl.CommonExtension
+import org.gradle.api.JavaVersion
+import org.gradle.api.Project
+import org.gradle.api.plugins.JavaPluginExtension
+import org.gradle.kotlin.dsl.configure
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+import org.jetbrains.kotlin.gradle.dsl.KotlinAndroidProjectExtension
+import org.jetbrains.kotlin.gradle.dsl.KotlinBaseExtension
+import org.jetbrains.kotlin.gradle.dsl.KotlinJvmProjectExtension
+
+internal fun Project.configureKotlinJvm() {
+    extensions.configure<JavaPluginExtension> {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+
+    configureKotlin<KotlinJvmProjectExtension>()
+}
+
+internal fun Project.configureKotlinAndroid(
+    commonExtension: CommonExtension<*, *, *, *, *, *>,
+) {
+    commonExtension.apply {
+        compileSdk = 35
+
+        defaultConfig {
+            minSdk = 28
+        }
+
+        compileOptions {
+            sourceCompatibility = JavaVersion.VERSION_11
+            targetCompatibility = JavaVersion.VERSION_11
+        }
+    }
+
+    configureKotlin<KotlinAndroidProjectExtension>()
+}
+
+private inline fun <reified T : KotlinBaseExtension> Project.configureKotlin() = configure<T> {
+    when (this) {
+        is KotlinAndroidProjectExtension -> compilerOptions
+        is KotlinJvmProjectExtension -> compilerOptions
+        else -> throw IllegalArgumentException("Unsupported Kotlin extension type: ${this::class.java}")
+    }.apply {
+        jvmTarget.set(JvmTarget.JVM_11)
+    }
+}
